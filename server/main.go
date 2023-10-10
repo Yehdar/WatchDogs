11c38bc1e050412c3f1d33de7b2ff94aa0d934c6
Yehdar <p.radhey06@gmail.com>
2023-10-10 11:44:30 -0400

    feat: add gRPC server initialization and start

--- server/main.go
+++ server/main.go
@@ -3,6 +3,8 @@ package main
 import (
   "log"
   "net"
+
+  "google.golang.org/grpc"
 )
 
 const (
@@ -13,5 +15,9 @@ func main() {
   lis, err := net.Listen("tcp", port)
   if err != nil {
     log.Fatalf("Failed to start the server: %v", err)
+  }
+  grpcServer := grpc.NewServer()
+  if err:= grpcServer.Serve(lis); err != nil {
+    log.Fatalf("Failed to start: %v", err)
   }
 }
