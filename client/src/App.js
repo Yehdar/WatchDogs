cafc885bd50b85e9c9b289f00607218d7de0c59a
Yehdar <p.radhey06@gmail.com>
2023-11-16 16:00:15 -0500

    build: correct folder name to client

--- client/src/App.js
+++ client/src/App.js
@@ -13,9 +13,14 @@ function App() {
   };
 
   return (
-    <div className="App" class="flex items-center justify-center min-h-screen">
+    <div
+      className="App"
+      class="flex items-center justify-center min-h-screen bg-slate-600"
+    >
       <div className="text-center">
-        <h1 class="text-3xl font-bold mb-4">Video Streaming Platform</h1>
+        <h1 class="text-3xl text-white font-bold mb-4">
+          Video Streaming Platform
+        </h1>
         <video width="700px" height="400px" controls>
           <source
             src="https://d2i8zdo0i9xncc.cloudfront.net/Kobe%20Bryant%20&%20Kanye%20West%20.mp4"
@@ -23,21 +28,21 @@ function App() {
           />
         </video>
         <div className="mt-4">
-          <h2 className="text-xl font-bold mb-2">Comments</h2>
-          <u1 className="list-disc">
+          <h2 className="text-xl text-slate-400 font-bold mb-2">Comments</h2>
+          <u1 className="list-disc text-slate-100">
             {comments.map((comment, index) => (
               <li key={index}>{comment}</li>
             ))}
           </u1>
           <div className="mt-4">
             <textarea
-              className="w-full p-2 border rounded"
-              placeholder="aaaaa"
+              className="w-full p-2 border rounded bg-slate-100"
+              placeholder="Add a comment..."
               value={newComment}
               onChange={(e) => setNewComment(e.target.value)}
             ></textarea>
             <button
-              className="mt-2 bg-blue-500 text-white p-2 rounded"
+              className="mt-2 bg-cyan-500 text-white p-2 rounded"
               onClick={addComment}
             >
               Add Comment
